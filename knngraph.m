383c34bf4b4d163cc893000c3d038f66ab6261ad
Trevor Vannoy <trevorvannoy@montana.edu>
2020-11-01 15:19:10 -0700

    documentation and formatting

--- knngraph.m
+++ knngraph.m
@@ -1,19 +1,28 @@
 function G = knngraph(X, k)
+%KNNGRAPH Construct a k-nearest neighbors graph
+%   G = KNNGRAPH(X, k) constructs a k-nearest neighbors directed graph, G,
+%   for the input data matrix X. Rows in X are observations, and columns are
+%   variables. The distance metric used is the Euclidean distance.
 
-    % rows are assumed to be observations, and columns are variables
-    [num_observations, ~] = size(X);
+arguments
+    X (:,:) double
+    k (1, 1) {mustBePositive, mustBeInteger}
+end
 
-    % knnsearch includes self-distances, so we use k + 1 to get k neighbors
-    neighbors = knnsearch(X, X, 'K', k + 1);
+% rows are assumed to be observations, and columns are variables
+[nObservations, ~] = size(X);
 
-    % create a sparse adjacency matrix 
-    A = sparse(num_observations, num_observations);
-    for i = 1:num_observations
-        A(i, neighbors(i, :)) = 1;
-        
-        % remove self-edges by setting diagonal elements to 0
-        A(i, i) = 0;
-    end
+% knnsearch includes self-distances, so we use k + 1 to get k neighbors
+neighbors = knnsearch(X, X, 'K', k + 1);
 
-    G = digraph(A);
+% create a sparse adjacency matrix 
+A = sparse(nObservations, nObservations);
+for i = 1:nObservations
+    A(i, neighbors(i, :)) = 1;
+    
+    % remove self-edges by setting diagonal elements to 0
+    A(i, i) = 0;
+end
+
+G = digraph(A);
 end
--- mutualknngraph.m
+++ mutualknngraph.m
@@ -1,17 +1,32 @@
 function G = mutualknngraph(X, k)
-    knn_graph = knngraph(X, k);
+%MUTUALKNNGRAPH Construct a mutual k-nearest neighbors graph
+%   G = MUTUALKNNGRAPH(X, k) constructs a mutual k-nearest neighbors undirected
+%   graph, G, for the input data matrix X. Rows in X are observations, 
+%   and columns are variables. The distance metric used is the Euclidean 
+%   distance.
+%
+%   The mutual k-nearest neighbors graph is a subgraph of the k-nearest 
+%   neighbors graph that only contains bidirectional edges, i.e. there is
+%   an edge between points p and q iff p is one of q's k-nearest neighbors and 
+%   q is also one of p's k-nearest neighbors.
 
-    % The mutual knn graph is a subgraph of the knn graph that only contains 
-    % bidirectional edges. To find bidirectional edges, we add the knn adjacency
-    % matrix to its transpose; entries with weight 2 in this new adjacency 
-    % matrix correspond to bidirectional edges in the knn graph. We then remove
-    % all unidirectional edges from the new adjacency matrix to get an adjacency
-    % matrix for the mutual knn graph. 
-    A = knn_graph.adjacency + knn_graph.adjacency.';
-    A(A==1) = 0;
+arguments
+    X (:,:) double
+    k (1, 1) {mustBePositive, mustBeInteger}
+end
+knnGraph = knngraph(X, k);
 
-    % normalize to weights of 1 so the undirected mutual knn graph is unweighted
-    A = A/2;
+% The mutual knn graph is a subgraph of the knn graph that only contains
+% bidirectional edges. To find bidirectional edges, we add the knn adjacency
+% matrix to its transpose; entries with weight 2 in this new adjacency
+% matrix correspond to bidirectional edges in the knn graph. We then remove
+% all unidirectional edges from the new adjacency matrix to get an adjacency
+% matrix for the mutual knn graph.
+A = knnGraph.adjacency + knnGraph.adjacency.';
+A(A==1) = 0;
 
-    G = graph(A);
+% normalize to weights of 1 so the undirected mutual knn graph is unweighted
+A = A/2;
+
+G = graph(A);
 end
