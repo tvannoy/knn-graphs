ba5d268ff0f3c9c66cafdea6e48193ed3a8b4dc7
Trevor Vannoy <trevorvannoy@montana.edu>
2020-11-13 22:04:04 -0700

    major performance improvement
    The sparse array indexing operations were *very* slow -- they took up the majority
    of the processing time. Constructing source node and target node arrays is about 4-5 times
    faster in a test with 50,000 100-dimensional observations

--- knngraph.m
+++ knngraph.m
@@ -15,14 +15,22 @@ end
 % knnsearch includes self-distances, so we use k + 1 to get k neighbors
 neighbors = knnsearch(X, X, 'K', k + 1);
 
-% create a sparse adjacency matrix
-A = sparse(nObservations, nObservations);
-for i = 1:nObservations
-    A(i, neighbors(i, :)) = 1;
-    
-    % remove self-edges by setting diagonal elements to 0
-    A(i, i) = 0;
-end
+% Turn the k-nearest neighbor indices returned by knnsearch into lists of 
+% source and target edges for the knn graph. Source nodes are repeated k + 1
+% times because there are k + 1 neighbors returned by knnsearch. When reshaping
+% the neighbor index matrix into an array, the neighbor matrix needs to be 
+% transposed because the neighbors are in rows but reshape goes by columns
+sourceNodes = repelem(1:nObservations, k + 1);
+targetNodes = reshape(neighbors', 1, []);
 
-G = digraph(A);
+% When subtracting sourceNodes from targetNodes, self-edges will show up as
+% zeros; Using find will return the indices of all non-zero entries, i.e. all
+% edges that aren't self-edges
+nonSelfEdges = find(targetNodes - sourceNodes);
+
+% remove self-edges
+sourceNodes = sourceNodes(nonSelfEdges);
+targetNodes = targetNodes(nonSelfEdges);
+
+G = digraph(sourceNodes, targetNodes);
 end
